bb67d065e37be0ac18a046a1b2531b4a3bd92b54
unknown <abebeyohannisooo@gmail.com>
2025-08-25 12:54:26 +0300

    Now all problems fixed

--- app/Components/ContentGrid.tsx
+++ app/Components/ContentGrid.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import ContentCard from './ContentCard';
 
-const ContentGrid = () => {
-  return (
-    <div>ContentGrid</div>
-  )
+interface ContentGridProps {
+  items: any[];
+  type: 'movie' | 'series' | 'documentary';
 }
 
-export default ContentGrid
+const ContentGrid: React.FC<ContentGridProps> = ({ items, type }) => {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      {items.map((item) => (
+        <ContentCard key={item.id} item={item} type={type} />
+      ))}
+    </div>
+  );
+};
+
+export default ContentGrid;
